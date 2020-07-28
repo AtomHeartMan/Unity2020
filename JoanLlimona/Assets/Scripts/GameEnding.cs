24d0a6b14c50b886de93e7d519f33aa91108242a
AtomHeartMan <68591657+AtomHeartMan@users.noreply.github.com>
2020-07-28 13:58:45 +0200

    John Lemon - Game Ending

---
+++ JoanLlimona/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayTimeDuration = 1f;
+    private bool isPlayerAtExit;
+    
+    public GameObject player;
+
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    private float timer;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            isPlayerAtExit = true;
+        }   
+    }
+
+    private void Update()
+    {
+        if (isPlayerAtExit)
+        {
+            timer += Time.deltaTime;
+            exitBackgroundImageCanvasGroup.alpha = Mathf.Clamp(timer / fadeDuration, 0, 1);
+        }
+
+        if (timer > fadeDuration +displayTimeDuration)
+        {
+            EndLevel();
+        }
+    }
+
+    /// <summary>
+    /// Desvanece el Canvas Group y finaliza el juego
+    /// </summary>
+    void EndLevel()
+    {
+        Debug.Log("Yes.");
+        Application.Quit();
+    }
+}
